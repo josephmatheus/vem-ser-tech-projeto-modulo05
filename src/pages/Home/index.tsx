6a7f9ce2d9f39983792f3f86d404ab7f56463a2e
Joseph Matheus <josephmatheus@hotmail.com>
2024-02-19 18:21:56 -0300

    :sparkles: feat: Componente página inicial <Home />

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+// import { Link } from "react-router-dom"
+
+import { RecipeList } from "../../components/RecipeList"
+
+export const Home = () => {
+ 
+  return (
+    <>
+    <RecipeList />
+    </>
+  )
+}
