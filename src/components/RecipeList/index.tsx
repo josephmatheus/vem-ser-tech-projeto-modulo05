23abcb512360701c5dcb7d9eb4a62de0cb52db0f
Joseph Matheus <josephmatheus@hotmail.com>
2024-02-21 15:46:21 -0300

    :recycle: refactor: Melhorias no Componente RecipeList

--- src/components/RecipeList/index.tsx
+++ src/components/RecipeList/index.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import recipesJson from "../../data/recipes.json";
 import Recipe from "../../types/Recipe";
 import { RecipeCard } from "../RecipeCard";
+import { Input, RecipeListContainer, StyledUl } from "./styled";
+import { IoSearch } from "react-icons/io5";
 
 export const RecipeList = () => {
-  const { recipes } = recipesJson
-  
+  const { recipes } = recipesJson;
+  const [search, setSearch] = useState("");
+
+  const filteredRecipes = recipes.filter(buscar);
+
+  function buscar(recipe: Recipe) {
+    return valorAtendeABusca(recipe.title);
+  }
+
+  function valorAtendeABusca(valor: string) {
+    return valor.toLowerCase().includes(search.toLowerCase());
+  }
+
   return (
-    <ul>
-      {recipes.map((recipe: Recipe, index: number) => {
-        return <RecipeCard key={index} recipe={recipe} />;
-      })}
-    </ul>
-  )
+    <RecipeListContainer>
+      <div className="search-bar-container">
+        <IoSearch color="#592507"  size={24} />
+        <Input
+          type="text"
+          placeholder="Pesquisar receita..."
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+      </div>
+      <StyledUl>
+        {filteredRecipes.map((recipe: Recipe, index: number) => {
+          return <RecipeCard key={index} recipe={recipe} />;
+        })}
+      </StyledUl>
+    </RecipeListContainer>
+  );
 };
